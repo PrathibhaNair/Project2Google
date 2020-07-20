8ce2c90b85003aeb61ce917149a9177760bde0fd
PrathibhaNair <prathibha.nair25@gmail.com>
2020-07-20 11:24:29 +0530

    Frankie's method added

--- src/test/java/org/test/GoogleMain.java
+++ src/test/java/org/test/GoogleMain.java
@@ -5,4 +5,8 @@ public class GoogleMain {
 	public static void main(String[] args) {
 		System.out.println("Google");
 	}
+	
+	public void frankieMethod() {
+		System.out.println("This is Frank's method...");
+	}
 }
