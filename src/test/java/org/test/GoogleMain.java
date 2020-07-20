9f8c6943debb1e6f2ea5a2cc5e5f78046db622cf
PrathibhaNair <prathibha.nair25@gmail.com>
2020-07-20 11:12:26 +0530

    Dimpy's changes to Google Maps

--- src/test/java/org/test/GoogleMain.java
+++ src/test/java/org/test/GoogleMain.java
@@ -4,5 +4,10 @@ public class GoogleMain {
 
 	public static void main(String[] args) {
 		System.out.println("Google");
+		
+	}
+	
+	public void googleMaps() {
+		System.out.println("This is for Google MApps app ... by dimpy");
 	}
 }
