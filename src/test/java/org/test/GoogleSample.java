13e10453c1e9d5d35436e443a3f36267119ec839
PrathibhaNair <prathibha.nair25@gmail.com>
2020-07-21 09:52:47 +0530

    Google trips code changes by Anu

--- src/test/java/org/test/GoogleSample.java
+++ src/test/java/org/test/GoogleSample.java
@@ -3,8 +3,10 @@ package org.test;
 public class GoogleSample {
 
 	public static void main(String[] args) {
-		
-			
 		System.out.println("Lines of code added by Anu...");
+	}
+	
+	public void googleTrips() {
+		System.out.println("Gogle trips app added by Anu..");
 	}
 }
